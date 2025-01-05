a873806e8414d15eef81f3903877d7190def3707
Sado4 <derasado04@gmail.com>
2025-01-05 22:49:21 +0900

    fix: マイクロサービス化(イベントブローカー使用して読み取りをクエリサービス化)

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -11,14 +14,28 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
   const postId = req.params.id;
   const comments = commentsByPostId[postId] || [];
   comments.push({ id: commentId, content });
   commentsByPostId[postId] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId,
+    },
+  });
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+  res.send({});
 });
 
 app.listen(4001, () => {
